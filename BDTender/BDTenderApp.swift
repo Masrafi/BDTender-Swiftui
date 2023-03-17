ccdf3f6c535609da792254555ed9023539aa7fd7
Masrafi <masrafianam@gmail.com>
2023-03-17 19:43:52 +0600

    Initial Commit

---
+++ BDTender/BDTenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BDTenderApp.swift
+//  BDTender
+//
+//  Created by user on 3/17/23.
+//
+
+import SwiftUI
+
+@main
+struct BDTenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
